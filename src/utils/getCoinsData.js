5711bf0f950c485cb30512f6879dd7ae265dc3e8
Tomasz Rzeznik <tom@gozdzik.local>
2020-11-17 21:16:50 +0100

    added unsaved changes

--- src/utils/getCoinsData.js
+++ src/utils/getCoinsData.js
@@ -1,7 +1,6 @@
 const { getCoinsDataWithQuantity, calculateCoinsQuantity } = require('./coinsQuantity.js');
 
 function getCoinData(tipCoins) {
-  console.log('getCoinsDataWithQuantity(calculateCoinsQuantity(tipCoins))', getCoinsDataWithQuantity(calculateCoinsQuantity(tipCoins)));
   return getCoinsDataWithQuantity(calculateCoinsQuantity(tipCoins));
 }
 
